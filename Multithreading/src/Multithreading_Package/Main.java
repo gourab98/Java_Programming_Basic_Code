912d2b245802bf32d629168ead0167f9ca68801e
Gourab <gourabsahag@gmail.com>
2019-03-27 12:02:50 +0600

    Four Commit

--- Multithreading/src/Multithreading_Package/Main.java
+++ Multithreading/src/Multithreading_Package/Main.java
@@ -10,15 +10,19 @@ package Multithreading_Package;
  * @author gourab
  */
 public class Main {
+    public static int glob_var=12;
+    
     public static void main(String[] args) {
+        
+        
         Multithreading t1 =new Multithreading("One");
         Multithreading t2 =new Multithreading("Two");
         
-        Thread t=new Thread(t1);
-        t.start();
+        Thread th1=new Thread(t1);
+        th1.start();
         
-        t=new Thread(t2);
-        t.start();
+        Thread th2 =new Thread(t2);
+        th2.start();
     }
     
 }
--- Multithreading/src/Multithreading_Package/Multithreading.java
+++ Multithreading/src/Multithreading_Package/Multithreading.java
@@ -4,19 +4,29 @@ public class Multithreading implements Runnable{
     
     String threadName=" ";
     Thread t;
+    private Object threadname;
     
     public Multithreading (String name)
     {
         this.threadName=name;
-        t =new Thread(this, name);
-        t.start();
+//        t =new Thread(this, name);
+//        t.start();
+    }
+    synchronized public void Operation(int add)
+    {
+        Main.glob_var+=add;
     }
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(threadName+" "+i);
-        }
+            if(!threadName.equalsIgnoreCase("One")) {
+                Operation(1);
+            } else {
+               Operation(-1);
+            }
+            System.out.println(Main.glob_var);
     }
     
+}
 }
